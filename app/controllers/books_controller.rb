ca00b81ecf6323ee9fcbd300a21ce728e09512ad
tomoprogrammer <train.vegacat@gmail.com>
2021-08-17 13:42:29 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(blog)
+    
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+
+end

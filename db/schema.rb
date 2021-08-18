00535723a70f6f2caa0e09de08a9fe76a40761a3
tomoprogrammer <train.vegacat@gmail.com>
2021-08-18 13:21:12 +0000

    Inital Commit

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,4 @@ ActiveRecord::Schema.define(version: 2021_08_17_054923) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
